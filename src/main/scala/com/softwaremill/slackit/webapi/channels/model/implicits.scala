08e9e797ef2876d932226e3a8ffd75aff2429386
Piotr Buda <pibuda@gmail.com>
2017-01-04 15:06:48 +0100

    Adds encoders for messages

--- src/main/scala/com/softwaremill/slackit/webapi/channels/model/implicits.scala
+++ src/main/scala/com/softwaremill/slackit/webapi/channels/model/implicits.scala
@@ -1,7 +1,8 @@
 package com.softwaremill.slackit.webapi.channels.model
 
 import io.circe.generic.auto._
-import io.circe.{Decoder, HCursor}
+import io.circe.{Decoder, Encoder, HCursor}
+import io.circe.syntax._
 
 object implicits {
   implicit val slackTimeStampDecoder: Decoder[SlackTimeStamp] = Decoder.decodeString.map(SlackTimeStamp(_))
@@ -9,9 +10,25 @@ object implicits {
   implicit val messageDecoder: Decoder[Message] = new Decoder[Message] {
     override def apply(c: HCursor) = {
       c.get[String]("subtype") match {
-        case Right("bot_message") => c.as[BotMessage]
-        case Right(_)             => c.as[SubtypedMessage]
-        case _                    => c.as[RegularMessage]
+        case Right("bot_message")  => c.as[BotMessage]
+        case Right("file_comment") => c.as[FileCommentMessage]
+        case Right(_)              => c.as[SubtypedMessage]
+        case _                     => c.as[RegularMessage]
+      }
+    }
+  }
+
+  implicit val slackTimeStampEncoder: Encoder[SlackTimeStamp] = new Encoder[SlackTimeStamp] {
+    override def apply(a: SlackTimeStamp) = a.toString.asJson
+  }
+
+  implicit val messageEncoder: Encoder[Message] = new Encoder[Message] {
+    override def apply(a: Message) = {
+      a match {
+        case msg: RegularMessage  => msg.asJson
+        case msg: SubtypedMessage => msg.asJson
+        case msg: BotMessage      => msg.asJson
+        case _                    => throw new Error(s"Could not encode message $a")
       }
     }
   }
